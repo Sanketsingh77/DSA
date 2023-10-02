72aa605b77cd0599013c59c5a06e49345b48a1dc
Sanketsingh77 <35081012+Sanketsingh77@users.noreply.github.com>
2023-10-03 00:35:44 +0530

    Update and rename PythaGorean.md to PythaGorean.cpp

---
+++ InterviewBit/Array/PythaGorean.cpp
@@ -0,0 +1,30 @@
+https://www.interviewbit.com/problems/pythagorean-triplets/
+
+
+ int count = 0;
+ int arr[A+1];
+ arr[0]=0;
+ for(int i=1;i<=A;i++){
+    arr[i]=i*i;
+ }
+ for(int i=A;i>=1;i--)
+ {
+    for(int j=1,k=A;j<k;)
+    {
+       
+        if((arr[j]+arr[k])==arr[i])
+        {
+            count++;
+            k--;
+            j++;
+        }
+        else if((arr[j]+arr[k])>arr[i])
+        {  
+        k--;
+        }
+        else
+        {  
+        j++;
+        }
+    }
+ }
