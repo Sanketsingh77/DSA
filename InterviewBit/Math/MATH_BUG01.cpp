460c864f7e05668a165cb9d1788bf1ed6a7a929b
Sanketsingh77 <35081012+Sanketsingh77@users.noreply.github.com>
2023-10-03 16:40:26 +0530

    Create MATH_BUG01.cpp

---
+++ InterviewBit/Math/MATH_BUG01.cpp
@@ -0,0 +1,24 @@
+/*
+Fix the bug in the below code:
+
+// Return 1 if A is prime, else 0
+int Solution::isPrime(int A) {
+    int upperLimit = (int)(sqrt(A));
+    for (int i = 2; i <= upperLimit; i++) {
+        if (i < A && A % i == 0) return 0;
+    }
+    return 1;
+}
+
+https://www.interviewbit.com/problems/mathbug01/
+*/
+
+int Solution::isPrime(int A) {
+    if (A == 1)
+        return 0;
+    int upperLimit = (int)(sqrt(A));
+    for (int i = 2; i <= upperLimit; ++i) {
+        if (A % i == 0) return 0;
+    }
+    return 1;
+}
