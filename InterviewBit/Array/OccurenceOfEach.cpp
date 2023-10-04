2e85afcc428849247b4c3a785b52b8a46914e8d2
Sanketsingh77 <35081012+Sanketsingh77@users.noreply.github.com>
2023-10-04 13:32:50 +0530

    Occurence of Each Number

---
+++ InterviewBit/Array/OccurenceOfEach.cpp
@@ -0,0 +1,12 @@
+//Occurence of Each Number
+
+https://www.interviewbit.com/problems/occurence-of-each-number/
+
+    map<int,int> rbmap;
+    for(int i = 0 ; i < A.size();i++)
+        rbmap[A[i]]++;
+    vector<int> ans;
+    for(auto i : rbmap)
+        ans.push_back(i.second);
+    return ans;
+
