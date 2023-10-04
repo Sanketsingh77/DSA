69da8dec389ebe79b71bea719bed783c3546e3dd
Sanketsingh77 <35081012+Sanketsingh77@users.noreply.github.com>
2023-10-04 14:10:22 +0530

    Array Sum

---
+++ InterviewBit/Array/ArraySum.cpp
@@ -0,0 +1,16 @@
+/* 
+https://www.interviewbit.com/problems/array-sum/
+*/
+
+vector<int> Solution::addArrays(vector<int> &A, vector<int> &B) {
+    if(A.size()<B.size()) A.swap(B); // making A bigger than B
+    int i=A.size()-1, j=B.size()-1, carry=0, sum=0;
+    while(i>=0){
+        sum = (j>=0) ? A[i]+B[j]+carry : A[i]+carry;
+        A[i] = sum%10;
+        carry = sum/10;
+        i--; j--;
+    }
+    if(carry) A.insert(A.begin(), carry);
+    return A;
+}
