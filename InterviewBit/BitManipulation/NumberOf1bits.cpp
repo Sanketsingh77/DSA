09be5e0909077531a53d579e624a869a3f73e9c7
Sanketsingh77 <35081012+Sanketsingh77@users.noreply.github.com>
2023-10-04 14:24:03 +0530

    Number of 1 bits

---
+++ InterviewBit/BitManipulation/NumberOf1bits.cpp
@@ -0,0 +1,16 @@
+// https://www.interviewbit.com/problems/number-of-1-bits/
+
+
+
+int Solution::numSetBits(unsigned int A) {
+    int count = 0;
+    while(A != 0)
+    {
+        if(A & 1)
+        {
+            count++;
+        }
+        A = A >> 1;
+    }
+    return count;
+}
