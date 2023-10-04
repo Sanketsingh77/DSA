779982ed4657823597671e23f5aa39432d2f2322
Sanketsingh77 <35081012+Sanketsingh77@users.noreply.github.com>
2023-10-04 14:31:40 +0530

    Reverse Bits

--- InterviewBit/BitManipulation/ReverseBits.cpp
+++ InterviewBit/BitManipulation/ReverseBits.cpp
@@ -2,6 +2,7 @@
 initializing result & getting rid off the leftmost digit at each iteration and adding it to the double (<< 1 works like * 2 in this case) 
 to previously computed result, at each iteration removing the just used leftmost bit with a right shift: >> 1.
 */
+
 unsigned int Solution::reverse(unsigned int A) {
     uint32_t result= 0;
     for(int i=0; i<32; i++)
