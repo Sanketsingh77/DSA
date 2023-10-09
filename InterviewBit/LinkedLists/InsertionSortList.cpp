a1a174a455667f3b139a0a27161cafd1b6ba8524
Sanketsingh77 <35081012+Sanketsingh77@users.noreply.github.com>
2023-10-09 23:29:55 +0530

    Insertion Sort List

---
+++ InterviewBit/LinkedLists/InsertionSortList.cpp
@@ -0,0 +1,19 @@
+// https://www.interviewbit.com/problems/insertion-sort-list/
+
+ListNode* Solution::insertionSortList(ListNode* A) {
+ListNode *temp=A;
+while(temp->next!=NULL)
+{
+    ListNode *a=temp->next;
+    while(a)
+    {
+        if(temp->val>a->val)
+        {
+            swap(a->val, temp->val);
+        }
+        a=a->next;
+    }
+    temp=temp->next;
+}
+return A;
+}
