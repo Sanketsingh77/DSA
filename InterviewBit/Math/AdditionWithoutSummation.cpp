d3478dfa23df7dc2f45ff5b4b792e453691ccf0d
Sanketsingh77 <35081012+Sanketsingh77@users.noreply.github.com>
2023-10-16 15:49:01 +0530

    Addition Without Summation

---
+++ InterviewBit/Math/AdditionWithoutSummation.cpp
@@ -0,0 +1,13 @@
+int add(int a,int b)
+{
+    for(int i=1;i<=b;i++)
+    {
+        a++;
+    }
+    return a;
+}
+int Solution::addNumbers(int A, int B) {
+    int ans=add(A,B);
+    return ans;
+
+}
