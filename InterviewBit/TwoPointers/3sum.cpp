14573a3ccf5dd19a7c5b0f171bcdc7eb3ee95b68
Sanketsingh77 <35081012+Sanketsingh77@users.noreply.github.com>
2023-10-17 17:29:16 +0530

    3 Sum

--- InterviewBit/TwoPointers/3sum.cpp
+++ InterviewBit/TwoPointers/3sum.cpp
@@ -19,17 +19,6 @@ int Solution::threeSumClosest(vector<int> &A, int B) {
     int result = 0;
     for (auto i = 0; i<n-2; ++i)
     {
-        /*int sum = 0; 
-        auto j = i;
-        while (j<j+3)       //My flawed thinking that sum can be of only consecutive elements
-            sum += A[j++];  //It could be A[i]+A[i+1]+A[n-1] also not necessarily alaways consecutive 3.
-        int diff2;
-        diff2 = (sum>B) ? (sum-B) : (B-sum);
-        cout << diff2 << " ";
-        if(diff2<diff)
-        {
-            result = sum; diff = diff2;
-        }*/
         int l = i+1, r = n-1;
         while (l<r)
         {
