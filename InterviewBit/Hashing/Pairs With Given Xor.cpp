aa8d13de6e98576726ed431d913026cc68b54ff7
Sanketsingh77 <35081012+Sanketsingh77@users.noreply.github.com>
2023-10-19 14:13:44 +0530

    Pairs With Given Xor

---
+++ InterviewBit/Hashing/Pairs With Given Xor.cpp
@@ -0,0 +1,17 @@
+//https://interviewbit.com/problems/pairs-with-given-xor/
+
+int Solution::solve(vector<int> &A, int B) {
+    unordered_map<int,int>m(A.size());
+    int ans=0;
+    for(int i=0;i<A.size();i++){
+        int j=A[i];
+        int k=A[i]^B;
+        if(m.find(k)!=m.end()){
+            ans++;
+        }
+        else{
+            m[j]=i;
+        }
+    }
+    return ans;
+}
