c1c8b78ebe672b6b52859b3cc559712004507411
Sanketsingh77 <35081012+Sanketsingh77@users.noreply.github.com>
2023-10-28 22:40:11 +0530

    Update ColorfulNumber.cpp

--- InterviewBit/Hashing/ColorfulNumber.cpp
+++ InterviewBit/Hashing/ColorfulNumber.cpp
@@ -5,6 +5,7 @@ Return 0/1
 
 COLORFUL number:
 
+
 A number can be broken into different contiguous sub-subsequence parts. 
 Suppose, a number 3245 can be broken into parts like 3 2 4 5 32 24 45 324 245. 
 And this number is a COLORFUL number, since product of every digit of a contiguous subsequence is different
